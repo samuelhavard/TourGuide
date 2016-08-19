86501dd3d408889cd0cb0c75e379f2713a0dd3ae
Samuel Havard <SamuelHavard@yahoo.com>
2016-08-19 07:26:50 -0400

    update adapters to show fragments

--- app/src/main/java/com/example/android/tourguide/Fragment/TabThreeFragment.java
+++ app/src/main/java/com/example/android/tourguide/Fragment/TabThreeFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.android.tourguide.Adapter.ItemAdapter;
 import com.example.android.tourguide.Classes.TourItem;
