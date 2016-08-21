73269fa8158abc624ff4adcd9163e221b97a79f6
Samuel Havard <SamuelHavard@yahoo.com>
2016-08-21 10:17:48 -0400

    Update Mipmap

--- app/src/main/java/com/example/android/tourguide/Fragment/TabFourFragment.java
+++ app/src/main/java/com/example/android/tourguide/Fragment/TabFourFragment.java
@@ -6,9 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.android.tourguide.Adapter.ItemAdapter;
 import com.example.android.tourguide.Classes.TourItem;
